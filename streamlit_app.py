2307fb3e6d4782e1119239b8f5a4875f033b116e
shirishamcoh <shirisha.guntaka@houstontx.gov>
2026-04-16 15:31:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,6 @@ if time_to_insert:
 
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
